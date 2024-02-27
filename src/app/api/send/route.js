29a17e60ede2ba482381c4d96ace453e1ac8061a
ShreeRam <shreeramshree109@gmail.com>
2024-02-27 18:54:00 +0530

    changes updated

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,16 +1,22 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
+// Check if the environment variables are available, otherwise provide default values
+const RESEND_API_KEY = process.env.RESEND_API_KEY || "your_default_api_key";
+const FROM_EMAIL = process.env.FROM_EMAIL || "your_default_email@example.com";
+
+// Initialize Resend instance with the API key
+const resend = new Resend(RESEND_API_KEY);
 
 export async function POST(req, res) {
   const { email, subject, message } = await req.json();
   console.log(email, subject, message);
+
   try {
+    // Send email using Resend
     const data = await resend.emails.send({
-      from: fromEmail,
-      to: [fromEmail, email],
+      from: FROM_EMAIL,
+      to: [FROM_EMAIL, email],
       subject: subject,
       react: (
         <>
@@ -21,8 +27,12 @@ export async function POST(req, res) {
         </>
       ),
     });
+
+    // Return success response
     return NextResponse.json(data);
   } catch (error) {
-    return NextResponse.json({ error });
+    // Handle error and return appropriate response
+    console.error("Error sending email:", error);
+    return NextResponse.json({ error: "Failed to send email." });
   }
 }
